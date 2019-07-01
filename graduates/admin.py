209ccd426c30c5289cf5eecb2a65bf7f3fbb2669
erezlevanon <erez.levanon@mail.huji.ac.il>
2019-07-01 23:20:18 +0300

    Create Class model

--- graduates/admin.py
+++ graduates/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Graduate
+from .models import Graduate, Class
 
+admin.site.register(Class)
 admin.site.register(Graduate)
--- graduates/models.py
+++ graduates/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 
-class Graduate(models.Model):
-    name = models.CharField(max_length=25)
+class Class(models.Model):
+    year = models.IntegerField()
 
     def __str__(self):
-        return self.name
+        return str(self.year)
+
+
+class Graduate(models.Model):
+    name = models.CharField(max_length=25)
+    year = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name + ' (' + str(self.year) + ')'
