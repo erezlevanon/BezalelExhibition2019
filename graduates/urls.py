0c9b53e73a11ac863bfbbc25cf548effd271ea10
erezlevanon <erez.levanon@mail.huji.ac.il>
2019-07-01 23:46:49 +0300

    avoid shadowing in views

--- graduates/urls.py
+++ graduates/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:year>', views.year, name='year'),
-    path('<int:year>/<str:name>', views.graduate, name='graduate'),
+    path('<int:class_year>', views.year, name='year'),
+    path('<int:class_year>/<str:name>', views.graduate, name='graduate'),
 ]
--- graduates/views.py
+++ graduates/views.py
@@ -4,12 +4,15 @@ from django.http import HttpResponse
 
 app_name = 'graduates'
 
+
 def index(requset):
     return HttpResponse("Index")
 
-def year(request, year):
-    return HttpResponse("welcome to " + str(year))
 
-def graduate(request, year, name):
-    print('heyyyyyyyyy ' + str(year) + " " + name)
+def year(request, class_year):
+    return HttpResponse("welcome to " + str(class_year))
+
+
+def graduate(request, class_year, name):
+    print('heyyyyyyyyy ' + str(class_year) + " " + name)
     return HttpResponse("Hello, world. You're at the polls index.")
