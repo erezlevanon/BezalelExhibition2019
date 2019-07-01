c43837c0c90d910daaecb4f3f3784b6e2d6eeb67
erezlevanon <erez.levanon@mail.huji.ac.il>
2019-07-01 23:45:15 +0300

    basic url dispatching

--- graduates/urls.py
+++ graduates/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:year>', views.year, name='year'),
+    path('<int:year>/<str:name>', views.graduate, name='graduate'),
 ]
--- graduates/views.py
+++ graduates/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+app_name = 'graduates'
 
-def index(request):
+def index(requset):
+    return HttpResponse("Index")
+
+def year(request, year):
+    return HttpResponse("welcome to " + str(year))
+
+def graduate(request, year, name):
+    print('heyyyyyyyyy ' + str(year) + " " + name)
     return HttpResponse("Hello, world. You're at the polls index.")
