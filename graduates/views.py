a971cd7d2f2a19c3d986b44baa2ec71a3940a498
erezlevanon <erez.levanon@mail.huji.ac.il>
2019-07-13 22:09:41 +0300

    Keep visited graduates in session

--- graduates/views.py
+++ graduates/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render, redirect
 from .models import Class, Graduate
 
 app_name = 'graduates'
+SESSION_GRADUATES_KEY = 'graduates'
 
 
 def index(requset):
-    return HttpResponse("Index")
+    viewed = requset.session.get(SESSION_GRADUATES_KEY, [])
+    return HttpResponse(viewed)
 
 
 def year(request, class_year):
@@ -15,5 +17,11 @@ def year(request, class_year):
 
 def graduate(request, class_year, name_en):
     y = Class.objects.get(year=class_year)
-    cur_graduate = Graduate.objects.get(year=y, name_en__iexact=name_en, )
+    cur_graduate = Graduate.objects.get(year=y, name_en__iexact=name_en)
+
+    # Update visited graduates list
+    graduates_set = set(request.session.get(SESSION_GRADUATES_KEY, []))
+    graduates_set.add(cur_graduate.name_en)
+    request.session[SESSION_GRADUATES_KEY] = list(graduates_set)
+
     return render(request, 'graduates/graduate.html', {'graduate': cur_graduate})
