9a8a8a4994a534e087dd52389855746cba87ddc7
erezlevanon <erez.levanon@mail.huji.ac.il>
2019-07-13 03:03:04 +0300

    ahhhh

--- bezalelid/urls.py
+++ bezalelid/urls.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from decouple import config
 
 urlpatterns = [
     path('', include('graduates.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if not config('DEPLOYED', cast=bool):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
