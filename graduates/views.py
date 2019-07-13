4354436667da48186f97cc77404512211ecae060
erezlevanon <erez.levanon@mail.huji.ac.il>
2019-07-13 22:25:22 +0300

    retrigger release

--- graduates/views.py
+++ graduates/views.py
@@ -19,7 +19,7 @@ def graduate(request, class_year, name_en):
     y = Class.objects.get(year=class_year)
     cur_graduate = Graduate.objects.get(year=y, name_en__iexact=name_en)
 
-    # Update visited graduates list
+    # Update visited graduates list.
     graduates_set = set(request.session.get(SESSION_GRADUATES_KEY, []))
     graduates_set.add(cur_graduate.name_en)
     request.session[SESSION_GRADUATES_KEY] = list(graduates_set)
