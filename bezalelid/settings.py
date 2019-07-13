c585acde9a3b53c1740ffcca61c226b5b8ea7f73
erezlevanon <erez.levanon@mail.huji.ac.il>
2019-07-13 03:26:42 +0300

    ahhhh

--- bezalelid/settings.py
+++ bezalelid/settings.py
@@ -153,6 +153,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+FILE_UPLOAD_MAX_MEMORY_SIZE = 262144000
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
